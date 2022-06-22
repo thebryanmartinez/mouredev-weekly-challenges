8c05b7abd592e88452be26f25b439691e6290996
Bryanmartz972 <bryanmartz972@gmail.com>
2022-06-22 11:55:09 -0600

    ✔️ COMPLETE: Challenge1 - Anagram completed

--- challenges/Challenge1.js
+++ challenges/Challenge1.js
@@ -19,3 +19,40 @@
  * It is NOT necessary to check that both words exist.
  * Two exactly the same words are not an anagram.
  */
+
+const isAnAnagram = (firstWord, secondWord) => {
+  // Variables
+  let isLetterInside = false, isWordAnagram = false;
+  
+  // Words to uppercase
+  [firstWord, secondWord] = [firstWord.toUpperCase(), secondWord.toUpperCase()]
+  
+  // If both words equal returns false as a response 
+  if (firstWord === secondWord) return false
+  
+  // Splitting of words by characters
+  [firstWord, secondWord] = [firstWord.split(''), secondWord.split('')]
+  
+  /* For loop explanation: 
+    Checks if every letter of the second word exists inside the first word
+    If it does, isLetterInside becomes true, else it continues as false
+    At the end of the second loop, if isletterInside is true, isWordAnagram becomes true 
+    and continues until each letter is inside the other word and isLetterInside always returns true at the end of the second loop
+    Resulting that if isWordAnagram is always true at the end of the loop, the function returns true as a response
+    If at the end of the second loop isLetterInside remains false, isWordAnagram becomes false and returns false as a response 
+  */
+  for (let i = 0; i < secondWord.length; i++){
+    isLetterInside = false
+    for(let j = 0; j < firstWord.length; j++){
+      if(firstWord[j] === secondWord[i]){
+        isLetterInside = true
+        break;
+      }
+    }
+    isLetterInside ? isWordAnagram = true : isWordAnagram = false;
+    if(!isWordAnagram) return false
+  }
+  return true
+}
+
+console.log(isAnAnagram('Murcielago', 'crema'))
