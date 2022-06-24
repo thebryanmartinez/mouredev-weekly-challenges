82b011acd5ed66117f3d17df54215483052a0ff1
Bryanmartz972 <bryanmartz972@gmail.com>
2022-06-23 18:39:25 -0600

    ✔️ COMPLETE: Challenge2 - Fibonacci completed

--- challenges/Challenge2.js
+++ challenges/Challenge2.js
@@ -17,3 +17,22 @@
  * The Fibonacci series is made up of a succession of numbers in which the next one is always the sum of the previous two.
  * 0, 1, 1, 2, 3, 5, 8, 13...
  */
+
+const fibonacciSecuence = () => {
+  // Variables
+  let sequence = [0, 1], number = 0;
+
+  /* For loop explanation:
+    In the variable number, it sums the number of the index of the array sequence with its next index
+    Making it in the first loop 0 + 1
+    Then this number is push into the sequence array and starting the loop again
+    Once the number is greater than 50, the function returns the sequence array which has the fibonacci sequence
+  */
+  for (let i = 0; true; i++) {
+    number = sequence[i] + sequence[i + 1]
+    if (number >= 50) return sequence
+    sequence.push(number)
+  }
+}
+
+module.exports = fibonacciSecuence
