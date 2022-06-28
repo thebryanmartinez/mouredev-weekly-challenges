bcdd631c2bc773f927ff400a75bce4d6f70e6b0f
Bryanmartz972 <bryanmartz972@gmail.com>
2022-06-27 22:42:13 -0600

    ✔️ COMPLETE: Challenge6 - Inverted chain completed

--- challenges/Challenge6.js
+++ challenges/Challenge6.js
@@ -13,5 +13,23 @@
  * Difficulty: EASY
  *
  * Statement: Create a program that reverses the order of a text string without using language functions that do it automatically.
- * - If we passed "Hello world" it would return "odnum aloH"
+ * - If we passed "Hello world" it would return "dlrow olleH"
  */
+
+const invertedChain = (textToReverse) => {
+  // Variable
+  let reversedChain = []
+
+  /* Start the for loop in the last index of the textToReverse and substract it with every iteration
+  Push into the reversedChain array letter by letter, back to front each letter of the textToReverse string */
+  for(let i = textToReverse.length; i >= 0; i--){
+    reversedChain.push(textToReverse[i])
+  }
+
+  // Join the letters to form the string together and return it
+  return reversedChain.join('')
+}
+
+console.log(invertedChain('Hello world'))
+
+module.exports = invertedChain
