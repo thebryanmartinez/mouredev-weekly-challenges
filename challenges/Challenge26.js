1e8d3d89a338ca06cb0ddd27f98df73003ba689a
Bryanmartz972 <bryanmartz972@gmail.com>
2022-07-05 13:21:45 -0600

    📦 NEW: New Challenge26 added

---
+++ challenges/Challenge26.js
@@ -0,0 +1,19 @@
+/* Español
+ * Reto #26
+ * CUADRADO Y TRIÁNGULO 2D
+ * Dificultad: FÁCIL
+ *
+ * Enunciado: Crea un programa que dibuje un cuadrado o un triángulo con asteriscos "*".
+ * - Indicaremos el tamaño del lado y si la figura a dibujar es una u otra.
+ * - EXTRA: ¿Eres capaz de dibujar más figuras?
+ */
+
+/* English
+ * Challenge #26
+ * SQUARE AND TRIANGLE 2D
+ * Difficulty: EASY
+ *
+ * Statement: Create a program that draws a square or a triangle with asterisks "*".
+ * - We will indicate the size of the side and if the figure to draw is one or the other.
+ * - EXTRA: Are you able to draw more figures?
+ */
