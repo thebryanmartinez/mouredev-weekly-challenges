e6ebb5785a72c2d562a683523d7478c0a4fda73f
Bryanmartz972 <bryanmartz972@gmail.com>
2022-07-13 14:47:33 -0600

    📦NEW: Eslint and Prettier setup

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    es2021: true
+  },
+  extends: [
+    'standard',
+    'eslint-config-prettier'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    'no-unused-vars': 'warn'
+  }
+}
